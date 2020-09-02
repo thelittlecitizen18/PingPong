fab6ce550cd47e1af24179882c8594e194a4b4a2
Your Name <thelittlecitizen18@gmail.com>
2020-09-02 21:27:44 +0300

    Server Return Messags To Client

---
+++ PingPong/Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            sender.Connect(remoteEP);
+
+            Console.WriteLine("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString());
+
+            while (true)
+            {
+                byte[] bytes1 = new byte[1024];
+                Console.WriteLine("Write a message:");
+                byte[] msg = Encoding.ASCII.GetBytes(Console.ReadLine());
+                sender.Send(msg);
+                int backmsg = sender.Receive(bytes1);
+                string msg2 = Encoding.ASCII.GetString(bytes1, 0, backmsg);
+                Console.WriteLine(msg2);
+            }
+
+            //byte[] bytes = new byte[1024];
+            //int bytesRec = handler.Receive(bytes);
+            //string data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            //byte[] msg = Encoding.ASCII.GetBytes(data);
+            //handler.Send(msg);
+
+
+
+
+
+
+
+            //int bytesSent = sender.Send(msg);
+            //int bytesRec = sender.Receive(bytes);
+            //Console.WriteLine("Echoed test = {0}",
+            //    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+            //sender.Shutdown(SocketShutdown.Both);
+            //sender.Close();
+
+
+
+
+        }
+    }
+}
---
+++ PingPong/Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Server!");
+
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+           
+            listener.Bind(localEndPoint);
+ 
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for a connection...");
+            Socket handler = listener.Accept();
+
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                string data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                Console.WriteLine("Return {0}", data);
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+
+            }
+
+
+
+
+
+
+
+            //string data = null;
+            //byte[] bytes = null;
+
+            //while (true)
+            //{
+            //    bytes = new byte[1024];
+            //    int bytesRec = handler.Receive(bytes);
+            //    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            //    if (data.IndexOf("<EOF>") > -1)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine("Text received : {0}", data);
+
+            //byte[] msg = Encoding.ASCII.GetBytes(data);
+            //handler.Send(msg);
+            //handler.Shutdown(SocketShutdown.Both);
+            //handler.Close();
+
+        }
+    }
+}
